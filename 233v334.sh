8dd2494a263fccea417e7de08f211bbd9b7b89c7
ron777-777 <57038401+ron777-777@users.noreply.github.com>
2022-03-29 02:43:04 +0800

    Update 233v334.sh

--- 233v334.sh
+++ 233v334.sh
@@ -10,7 +10,7 @@ fi
 # 防止处于 /tmp/233boy/v2ray 目录
 cd
 rm -rf /tmp/233boy/v2ray
-git clone https://github.com/crazypeace/v2ray -b "master" /tmp/233boy/v2ray --depth=1
+git clone https://github.com/ron777-777/v2ray -b "master" /tmp/233boy/v2ray --depth=1
 cd /tmp/233boy/v2ray
 chmod +x install.sh
 ./install.sh local
